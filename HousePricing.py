9333dcfb928660658eb833dd996a671190f1fd95
Joona Luoma <joona.luoma@solteq.com>
2017-11-05 18:55:29 +0200

    Add multicollinearity and outlier handling

--- HousePricing.py
+++ HousePricing.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 import random
+import seaborn as sns
+import matplotlib as plt
+from scipy import stats
 from sklearn.linear_model import LassoCV, LogisticRegressionCV, Ridge
 from sklearn.model_selection import cross_val_score, KFold, GridSearchCV
 from sklearn.preprocessing import StandardScaler
@@ -50,11 +53,44 @@ def analyse():
 
     train, test = load()
     all_data = pd.concat((train, test)).reset_index()  # type: pd.DataFrame
+    all_data_orig = all_data.copy()
 
     # Add new features:
     # * Descriptive_GrLivArea
     all_data['Descriptive_GrLivArea'] = all_data['GrLivArea']\
         .map(lambda v: 'small' if v < 844 else 'medium' if v < 1761 else 'large')
+    # Descriptive_TotalBsmtSF
+    all_data['Descriptive_TotalBsmtSF'] = all_data['TotalBsmtSF']\
+        .map(lambda v: 'small' if v < 960 else 'medium' if v < 1770 else 'large')
+
+    # Check correlations
+    # Credits to https://www.kaggle.com/pmarcelino/comprehensive-data-exploration-with-python
+    corr_mat = all_data.corr()
+    ax = sns.heatmap(corr_mat, square=True)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+    ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
+
+    # Check correlations of top n features
+    top_n = 10
+    cols = corr_mat.nlargest(top_n, 'SalePrice')['SalePrice'].index
+    ax = sns.heatmap(corr_mat.loc[cols, cols])
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=22)
+    ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
+
+    # Multicollinearity present in
+    # * CarageCars and CarageArea -> Choose CarageArea
+    # * TotalBsmtSF and 1stFlrSF -> Choose 1stFlsSF
+    multicoll_cols = ['GarageCars', 'TotalBsmtSF']
+    cols = cols[~cols.isin(multicoll_cols)]
+    all_data = all_data.loc[:, ~all_data.columns.isin(multicoll_cols)]
+
+    # Check with scatterplots
+    # * correlations
+    # * outliers
+    sns.pairplot(all_data[cols].dropna(), size=2.5)
+    # Drop from train
+    # * GrLivArea > 4000
+    # * 1stFlrSF > 4000
 
     # Plot SalePrice histogram
     train.loc[:,'SalePrice'].plot(kind='hist')
@@ -66,6 +102,9 @@ def analyse():
 
     # Check other numeric variables too for skewedness.
     skewed_feats = get_skewed_feats(all_data)
+    # Check the most significant skewed feats
+    sign_skewed_feats = skewed_feats[skewed_feats.isin(cols)]
+    ax = plt.pyplot.subplot()
 
     # Take GrLivArea as an example of a skewed feature.
     all_data.loc[:,'GrLivArea'].plot(kind='hist')
@@ -80,8 +119,6 @@ def analyse():
     # Generate dummies for categoricals
     all_data = pd.get_dummies(all_data)
 
-    # TODO: impute categorical variable missing values according to the documentation
-
     # Check missing value amounts
     missing_feats = get_missing_feats(all_data)
     # Drop features with too many missing values
@@ -89,8 +126,13 @@ def analyse():
     # Still some missing values. Impute with median.
     all_data = all_data.fillna(all_data.median())
 
-    train = all_data.loc[slice(0,len(train)-1),:]
+    train = all_data.loc[slice(0,len(train)-1),:] # type: pd.DataFrame
+    # Drop outliers
+    train = train.drop(train[train['GrLivArea'] > 4000].index)
+    train = train.drop(train[train['1stFlrSF'] > 4000].index)
+
     test = all_data.loc[slice(len(train), len(all_data)),:]
+    test = test.loc[:, test.columns != 'SalePrice']
 
     X = train.loc[:,train.columns!='SalePrice']
     y = train.loc[:,'SalePrice']
@@ -109,38 +151,10 @@ def analyse():
     ax = top_coefs.plot(kind='bar')
     ax.set_xticklabels(top_coefs['name'], rotation=45)
 
-    # Normalized with StandardScaler, the following params seem to affect the dependant variable the most
-    # * 1st tier
-    # 	* GrLivArea
-    # * 2nd tier
-    # 	* TotalBsmtSF
-    # 	* BsmtCond_TA
-    # * 3rd tier
-    # 	* BsmtCond_Gd
-    # 	* BsmtCond_Fa
-    # 	* YearBuilt
-    # 	* OverallQual
-    # 	* LotArea
-    # 	* RoofMatl_ClyTile
-    # 	* BsmtFinSF1
-    # 	* OverallCond
-    # 	* PoolArea
-    # 	* BsmtQual_TA
-    # 	* BsmtQual_Gd
-    # 	* MSZoning_C (all)
-
-    X['GrLivArea'].describe()
-    X['GrLivArea'].plot(kind='hist')
-
-    # Create Descriptive_GrLivArea:
-    # * small < 844
-    # * 844 <= medium < 1761
-    # * 1761 <= large
-
     model = Ridge()
-
     scores = cross_val_score(model, X, y, cv=KFold())
 
+    model.fit(X, y)
     pred = model.predict(test)
     pred = np.expm1(pred)
     save(pd.DataFrame({'SalePrice': pred, 'Id': test['Id']}))
